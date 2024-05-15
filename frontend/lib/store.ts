fa11f72e227ddfe1d0b6a2db006959922a40f34f
yayamura <yutaka.ayamura@beeskynet.com>
2024-05-15 20:51:55 +0900

    gpt-4-o→gpt-4o

--- frontend/lib/store.ts
+++ frontend/lib/store.ts
@@ -6,7 +6,7 @@ export const modelsGens: { [key: string]: string } = {
   "gpt-4-1106-preview": "gpt-4-turbo",
   "gpt-4-0125-preview": "gpt-4-turbo",
   "gpt-4-turbo-2024-04-09": "gpt-4-turbo",
-  "gpt-4o-2024-05-13": "gpt-4-o",
+  "gpt-4o-2024-05-13": "gpt-4o",
   "anthropic.claude-instant-v1": "claude-instant-v1",
   "anthropic.claude-v2": "claude-v2",
   "anthropic.claude-v2:1": "claude-v2.1",

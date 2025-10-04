455b1aa0909678716c2e478eeacb3d1783472996
ytkayamura <yutaka.ayamura@beeskynet.com>
2025-10-04 15:00:30 +0900

    sonnet 3.7削除

--- frontend/lib/store.ts
+++ frontend/lib/store.ts
@@ -81,6 +81,7 @@ const modelsToDelete = [
   "claude-3-haiku-20240307",
   "claude-3-5-sonnet-20240620",
   "claude-3-5-sonnet-20241022",
+  "claude-3-7-sonnet-20250219",
   "claude-3-opus-20240229",
 ];
 modelsToDelete.forEach((model) => delete models[model]);
